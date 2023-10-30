725a244ddb8b8af6d71dfbef8a17b2e191479099
golden <amdexec@gmail.com>
2023-10-30 19:56:18 +0330

    netilfy.toml

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:3001");
+// const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://socketiochatapp-0tpc.onrender.com/");
 
 function App() {
   const [username, setUsername] = useState("");
