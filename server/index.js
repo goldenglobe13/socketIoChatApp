45afe441a9c337413147bf6edb3e5151277d0ad3
golden <amdexec@gmail.com>
2023-10-30 20:59:43 +0330

    credentials set to true

--- server/index.js
+++ server/index.js
@@ -12,9 +12,12 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:3000",
-    // origin: "https://master--warm-pegasus-40c82a.netlify.app/",
+    origin: "https://master--warm-pegasus-40c82a.netlify.app/",
     methods: ["GET", "POST"],
     credentials: true,
+    allowedHeaders: [
+      "Access-Control-Allow-Origin:https://master--warm-pegasus-40c82a.netlify.app/",
+    ],
   },
 });
 
